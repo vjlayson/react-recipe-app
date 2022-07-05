1ea6345f29446794c2e2f7108d04b74ce5b5f346
vjlayson <vjlayson@gmail.com>
2022-07-05 21:02:31 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  color: yellow;
+  background-color: red;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px #555;
+`;
+
+const AppNameComponent = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+`;
+
+const AppIcon = styled.img`
+  width: 60px;
+  height: 60px;
+  margin: 15px;
+`;
+
+const SearchComponent = styled.div`
+display: flex;
+flex-direction: row;
+background-color: white;
+padding: 10px;
+border-radius: 6px;
+width: 50%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppNameComponent>
+          <AppIcon src="/crab.svg" />
+          Recipe App
+        </AppNameComponent>
+        <SearchComponent>
+          <img src="/search-icon.svg" />
+          <input />
+        </SearchComponent>
+      </Header>
+      Vanessa Joyo
+    </Container>
   );
 }
 
